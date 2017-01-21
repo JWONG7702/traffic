0bfb69bee20f0df6a313febb1698a66a8856de56
Avik Laha <al3510@moose.cc.columbia.edu>
2017-01-21 01:23:50 -0500

    ok

--- 2/simulation.cpp
+++ 2/simulation.cpp
@@ -1,11 +1,18 @@
 #include "road.h"
 int main(){
-    Car* c = new Car(3);
-    c->accel(9);
-    Lane* l = new Lane(2);
-    l->carr[0] = c;
-    l->carr[1] = c;
+    Car* c1 = new Car(3);
+    c1->accel(9);
+    Car* c2 = new Car(3);
+
+    Lane* l1 = new Lane(200);
+    l->carr[0] = c1;
+    l->carr[100] = c2;
+    Lane* l2 = new Lane(100);
     
+    Road** r = new Road(2);
+    r->larr[0] = l1;
+    r->larr[1] = l2;
+
     for (int i = 0; i< 2; i++){
         cout << l->carr[i]->velocity<<endl;
     }

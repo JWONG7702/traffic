1b71d7a6c341291c489e9f6971aef40fb666b5ea
Avik Laha <al3510@moose.cc.columbia.edu>
2017-01-21 00:54:15 -0500

    ayyyyy

---
+++ anun/simulation.cpp
@@ -0,0 +1,14 @@
+#include "road.h"
+int main(){
+    Car* c = new Car(3);
+    c->accel(9);
+    Lane* l = new Lane(2);
+    l->carr[0] = c;
+    l->carr[1] = c;
+    
+    for (int i = 0; i< 2; i++){
+        cout << l->carr[i]->velocity<<endl;
+    }
+}
+//anun, pass it on
+//anoon, pass it on

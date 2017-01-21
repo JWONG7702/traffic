fa524937b8aa2f705acfe9b0ebc51e68053542b3
Abhishek Kodumagulla <ak3831@columbia.edu>
2017-01-21 03:24:18 -0500

    basically did some shit you know

--- 2/road.cpp
+++ 2/road.cpp
@@ -21,6 +21,7 @@ Road& Road::accelerate(){
     return *this;
 }
 
+//slows a car down if it's about to crash into the thing in front of it, and if it can
 Road& Road::slow(){
     for(int i = 0; i < this->length(); i++){
         for(int j = 0; j < this->larr()[i]->size(); j++){
@@ -37,6 +38,7 @@ Road& Road::slow(){
     
 }
 
+//randomly changes the velocity of some cars to be lower, based on a phantomProbability
 Road& Road::random(){
     for(int i = 0; i < this->count; i++){
         for(int j = 0; j < this->larr[i]->size; j++){
@@ -54,6 +56,7 @@ Road& Road::random(){
     return *this;
 }
 
+//for all the cars that haven't done their action (if they haven't changed lanes/merged) are just moved forward by their velocity
 Road& Road::motion(){
     for(int i = 0; i < this->count; i++){
         for(int j = this->larr[i]->size-1; j >= 0; j++){
@@ -75,6 +78,15 @@ Road& Road::motion(){
     return *this;
 }
 
+//handles merges and lane changes
+/*
+ Car checks maximum speed it can achieve on it's current position (x, lane) and adjacent lane (x, lane+1).
+ If the potential maximal speed on lane+1 is higher it checks safe conditions:
+ There is no car car on (x, lane+2) to avoid collision caused by two parallel cars changing lane to lane+1.
+ Distance to previous car on lane+1 is greater that it's speed to avoid emergency braking of previous car.
+ Change lane with probability P.
+
+ */
 Road& Road::merge(){
     
 }
@@ -94,11 +106,14 @@ Road& Road::clearDones(){
 }
 
 
+//advances the entire simulation (the road) forward one time step
 Road& Road::next()
 {
     this->accelerate();
     this->slow();
     this->random();
+    this->merge();
     this->motion();
+    this->clearDones();
     return *this;
 }

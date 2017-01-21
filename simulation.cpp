d85881c9168e7a695e7bdf746173ab079b09ca35
JWONG7702 <JWONG7702@users.noreply.github.com>
2017-01-20 23:14:01 -0500

    simulation.cpp
    start

---
+++ simulation.cpp
@@ -0,0 +1,42 @@
+#include <cstdio>
+#include <iostream>
+#include <vector>
+#include <memory>
+using namespace std;
+class Car {
+    public:
+        int velocity;
+        Car(int v){velocity = v;}
+        ~Car();
+        void accel( int i) { velocity += i;}
+        friend ostream& operator<<(ostream& os, Car& c){os<< "[" << c.velocity << "]"; return os;};
+};
+class Lane {
+    public:
+    //vector of sharedpointer of cars
+       int size;
+       Car** carr;
+       Lane(int l){ carr = new Car*[l]; size = l;}
+};
+
+class Road {
+    public:
+        int count;
+        Lane** larr;
+        Road(int b){ larr = new Lane*[b]; count = b;}
+        void addLane(Lane* lpt, int i){
+            larr[i] = &(*lpt);
+        }
+};
+
+int main(){
+    Car* c = new Car(3);
+    c->accel(9);
+    Lane* l = new Lane(2);
+    l->carr[0] = c;
+    l->carr[1] = c;
+    
+    for (int i = 0; i< 2; i++){
+        cout << l->carr[i]->velocity<<endl;
+    }
+}

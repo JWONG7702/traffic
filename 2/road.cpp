b909f07b9d04749bb69bb98508056b5cd68e69df
Abhishek Kodumagulla <ak3831@columbia.edu>
2017-01-21 02:21:07 -0500

    shit

--- 2/road.cpp
+++ 2/road.cpp
@@ -2,20 +2,43 @@
 
 #include "road.h"
 
+static int maxVel = 20;
+
 Road& Road::accelerate(){
-    
+    for(int i = 0; i < this->count; i++){
+        for(int j = 0; j < this->larr[i]->size; j++){
+            Car* c = this->larr[i]->carr[j];
+            if(c != 0){
+                if(c->velocity() < maxVel){
+                    c->accel();
+                }
+            }
+        }
+    }
+    return *this;
 }
 
 Road& Road::slow(){
+    for(int i = 0; i < this->count; i++){
+        for(int j = 0; j < this->larr[i]->size; j++){
+            Car* c = this->larr[i]->carr[j];
+            if(c != 0){
+                if(c->velocity() < maxVel){
+                    c->accel(); //PUSH MY SHIT
+                }
+            }
+        }
+    }
+    return *this;
     
 }
 
 Road& Road::random(){
-    
+    return *this;
 }
 
 Road& Road::motion(){
-    
+    return *this;
 }
 
 

d24ecbda60fe2118fee745c6f3b113d7af6a41ac
Avik Laha <al3510@moose.cc.columbia.edu>
2017-01-21 01:31:42 -0500

    slight change to tester

--- 2/simulation.cpp
+++ 2/simulation.cpp
@@ -5,16 +5,18 @@ int main(){
     Car* c2 = new Car(3);
 
     Lane* l1 = new Lane(200);
-    l->carr[0] = c1;
-    l->carr[100] = c2;
+    l1->carr[0] = c1;
+    l1->carr[100] = c2;
     Lane* l2 = new Lane(100);
     
-    Road** r = new Road(2);
+    Road* r = new Road(2);
     r->larr[0] = l1;
     r->larr[1] = l2;
 
-    for (int i = 0; i< 2; i++){
-        cout << l->carr[i]->velocity<<endl;
+    for(int i = 0; i<r->count; i++) {
+	for(int j=0; j<r->larr[i]->size; j++) {
+            if(Car* it = r->larr[i]->carr[j]) cout << it->velocity<<endl;
+        }
     }
 }
 //anun, pass it on

447dfd92dfe562e9b7f917774f3f8596280788e1
7702 <justinwong7702@gmail.com>
2017-01-22 00:42:42 -0500

    test2

--- 2/test2.cpp
+++ 2/test2.cpp
@@ -1,5 +1,6 @@
 #include "road.h"
 #include <iostream>
+#include <list>
 using namespace std;
 int main (){
     cout << "woeifn" << endl;
@@ -8,17 +9,25 @@ int main (){
     Car* c2 = new Car(0,0,3);
     cout << *c2 << *c1 << endl;
  
-    Lane* l1 = new Lane(200);
+    Lane* l1 = new Lane(20);
     l1->carr()[0] = c1;
-    l1->carr()[100] = c2;
-    Lane* l2 = new Lane(100);
+    l1->carr()[10] = c2;
+    Lane* l2 = new Lane(10);
     cout << *l1 << endl;
     Lane** arroflanes = new Lane*();
     arroflanes[0] = l1;
     arroflanes[1] = l2;
     Road *r = new Road(arroflanes, 2);
-    Road street = new Road(*r);
     cout << *r <<endl;    
-
+    
+    list<Road> simulation =*( new list<Road>());
+    for (int i = 0; i < 10; i++){
+        simulation.push_front(*r);
+        r = &(r->next());
+    } 
+    for (auto it = simulation.begin(); it != simulation.end(); it++){
+        cout << *it<< endl;
+    } 
+    
 
 }
